858ffbfaecffeea36a018da48baf605e44403628
Jodi-Ann Moore-Barrett <moorebarrett.jodiann@gmail.com>
2023-06-02 10:54:30 -0500

    Setup Initial Component Layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './stylesheet/index.css';
+import Banner from './components/Banner';
+import Button from './components/Button';
+import Footer from './components/Footer';
+import Gallery from './components/Gallery';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
+      <Header />
       <main>
         <div className="container">
-
+          <Banner
+            button={Button}
+          />
+          <Gallery />
         </div>
       </main>
+      <Footer />
     </>
   );
 }
